a702945268e29eeba964eafe41ecd0dece34072b
Pablo Laborde <p5.tacoronte@gmail.com>
2025-06-22 08:22:11 -0300

    0

--- naive_malloc.c
+++ naive_malloc.c
@@ -15,20 +15,21 @@
 */
 void *naive_malloc(size_t size)
 {
-	static void *lim = NULL, *cur_mem = NULL, *next_mem = NULL;
+	static void *next_mem;
+	void *lim = NULL, *cur_mem = NULL;
 	intptr_t dif = 0;
-	size_t  *header = NULL;
+	size_t *header = NULL;
 
 	lim = sbrk(0);
 	if (!next_mem)
-		next_mem = (void *)((intptr_t)lim + SS);
+		next_mem = lim;
 	dif = (intptr_t)lim - (intptr_t)next_mem;
-	if ((size_t)dif < size)
+	if ((size_t)dif < (size + SS))
 		if (brk((void *)((intptr_t)lim + SS + size)) == -1)
 			return (NULL);
-	cur_mem = next_mem;
-	next_mem = (void *)((intptr_t)cur_mem + size + SS);
-	header = (size_t *)((intptr_t)cur_mem - SS);
+	header = (size_t *)next_mem;
 	*header = size;
+	cur_mem = (void *)((intptr_t)next_mem + SS);
+	next_mem = (void *)((intptr_t)cur_mem + size);
 	return (cur_mem);
 }
