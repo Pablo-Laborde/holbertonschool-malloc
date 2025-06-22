8e5d745f9ef6cdd1618fe0a56e8dbeca7f418095
Pablo Laborde <p5.tacoronte@gmail.com>
2025-06-22 18:33:12 -0300

    test main

--- main.c
+++ main.c
@@ -3,30 +3,60 @@
 
 
 /**
+* write_to - func
+* @ptr: void *
+*/
+void write_to(void *ptr)
+{
+	char *str = (char *)ptr, *hol = "Holberton";
+	int i = 0;
+
+	for (; hol[i]; i++)
+		str[i] = hol[i];
+}
+
+
+/**
 * main - func
 * Return: int
 */
 int main(void)
 {
-	void *pos = NULL, *lim = NULL, *dif = NULL, *lim2 = NULL;
+	void *mem = NULL, *lim = NULL;
+	intptr_t pos = 0, cur_break = 0, start = 0;
 
 	lim = sbrk(0);
-	printf("current break: %p\n", lim);
+	printf("beginning: %p\n\n", lim);
+	start = (intptr_t)sbrk(0) - (intptr_t)lim;
 
-	pos = naive_malloc(24);
-	dif = (void *)((intptr_t)pos - (intptr_t)lim);
-	printf("pos: %p\n", (void *)dif);
+	mem = naive_malloc(16);
+	write_to(mem);
+	cur_break = (intptr_t)sbrk(0) - (intptr_t)lim;
+	pos = (intptr_t)mem - (intptr_t)lim - start;
+	printf("%#010lx -> %s\n", pos, (char *)mem);
+	printf("current break: %p\n", (void *)cur_break);
 
-	lim2 = sbrk(0);
-	printf("current break: %p\n", (void *)((intptr_t)lim2 - (intptr_t)lim));
+	mem = naive_malloc(16);
+	write_to(mem);
+	cur_break = (intptr_t)sbrk(0) - (intptr_t)lim;
+	pos = (intptr_t)mem - (intptr_t)lim - start;
+	printf("%#010lx -> %s\n", pos, (char *)mem);
+	printf("current break: %p\n", (void *)cur_break);
 
-	pos = naive_malloc(24);
-	dif = (void *)((intptr_t)pos - (intptr_t)lim);
-	printf("pos: %p\n", (void *)dif);
+	mem = naive_malloc(16);
+	write_to(mem);
+	cur_break = (intptr_t)sbrk(0) - (intptr_t)lim;
+	pos = (intptr_t)mem - (intptr_t)lim - start;
+	printf("%#010lx -> %s\n", pos, (char *)mem);
+	printf("current break: %p\n", (void *)cur_break);
 
-	pos = naive_malloc(24);
-	dif = (void *)((intptr_t)pos - (intptr_t)lim);
-	printf("pos: %p\n", (void *)dif);
+	mem = naive_malloc(16);
+	write_to(mem);
+	cur_break = (intptr_t)sbrk(0) - (intptr_t)lim;
+	pos = (intptr_t)mem - (intptr_t)lim - start;
+	printf("%#010lx -> %s\n", pos, (char *)mem);
+	printf("current break: %p\n", (void *)cur_break);
 
-	return(0);
+
+	return (0);
 }
