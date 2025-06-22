7a614417572ef1eff14f05e1f3fc345bb64b6b24
Pablo Laborde <p5.tacoronte@gmail.com>
2025-06-22 18:32:46 -0300

    0

--- naive_malloc.c
+++ naive_malloc.c
@@ -3,8 +3,7 @@
 
 #define SS sizeof(size_t)
 
-#define KB 1024
-#define MB (KB * 1024)
+#define PS sysconf(_SC_PAGESIZE)
 
 
 
@@ -25,12 +24,35 @@ void *naive_malloc(size_t size)
 		next_mem = lim;
 	dif = (intptr_t)lim - (intptr_t)next_mem;
 	if ((size_t)dif < (size + SS))
-		if (brk((void *)((intptr_t)lim + 4 * KB)) == -1)
+		if (brk((void *)((intptr_t)lim + PS)) == -1)
 			return (NULL);
-	/*
-	*	if (brk((void *)((intptr_t)lim + SS + size)) == -1)
-	*		return (NULL);
-	*/
+	header = (size_t *)next_mem;
+	*header = size;
+	cur_mem = (void *)((intptr_t)next_mem + SS);
+	next_mem = (void *)((intptr_t)cur_mem + size);
+	return (cur_mem);
+}
+
+
+/**
+* exact_malloc - func
+* @size: size_t
+* Return: void *
+*/
+void *exact_malloc(size_t size)
+{
+	static void *next_mem;
+	void *lim = NULL, *cur_mem = NULL;
+	intptr_t dif = 0;
+	size_t *header = NULL;
+
+	lim = sbrk(0);
+	if (!next_mem)
+		next_mem = lim;
+	dif = (intptr_t)lim - (intptr_t)next_mem;
+	if ((size_t)dif < (size + SS))
+		if (brk((void *)((intptr_t)lim + SS + size)) == -1)
+			return (NULL);
 	header = (size_t *)next_mem;
 	*header = size;
 	cur_mem = (void *)((intptr_t)next_mem + SS);
